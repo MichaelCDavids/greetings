12c880b835441572293b84a842a34a81450b8431
michael <michaelcdavids@gmail.com>
2018-05-04 14:40:00 +0200

    Changed the greetings function to create  a map of users greeted

--- js/GreetingsFactory.js
+++ js/GreetingsFactory.js
@@ -1,33 +1,52 @@
-function GreetingsFactory(){
+function GreetingsFactory(storedUsers){
+
+
 
   var counterVariable = 0;
+  var namesGreeted = storedUsers || {};
+
 
   function greetings(name,language){
-    if(language==='English'){
-      return "Hello, "+name;
-    }else if (language==='Afrikaans'){
-      return "Goeie dag, "+name;
-    }else if (language==='isiXhosa'){
-      return "Molo, "+name;
+      if(name !== ""){
+        if (namesGreeted[name] === undefined){
+        namesGreeted[name] = 0;
+        }
+        if(language==='English'){
+          return "Hello, "+name;
+        }else if (language==='Afrikaans'){
+          return "Goeie dag, "+name;
+        }else if (language==='isiXhosa'){
+          return "Molo, "+name;
+        }
+      }  
+    }
+  function greetedMap(user){
+    if (namesGreeted[user] === undefined){
+      namesGreeted[user] = 0;
     }
   }
   function resetCounter(){
     localStorage.clear();
   }
-  
+
   function counter(){
     counterVariable++;
     return counterVariable;
   }
 
   function totalCount(){
-    return counterVariable;
+    return Object.keys(namesGreeted).length;
   }
+  function getMap(){
+    return namesGreeted;
+  }
+
 
   return{
     greet : greetings,
     count : counter,
     getCountTotal: totalCount,
+    userMap : getMap,
 
     reset : resetCounter
   }
