0b7146c27b6c3a0fe2d7800c380ac45cc7ecbe93
michael <michaelcdavids@gmail.com>
2018-05-04 14:41:22 +0200

    Made the counter load from local storage

--- js/greetings.js
+++ js/greetings.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
+
+
   var nameBox = document.querySelector('.name-box');
   var greetSettings = document.querySelector('.greetSettings');
   var greetBtn  = document.querySelector('.greetButton');
@@ -6,30 +8,27 @@ document.addEventListener('DOMContentLoaded',function(){
   var counterSection = document.querySelector('.counter');
   var resetBtn = document.querySelector('.resetButton');
 
-
-  var greetCounter = 0;
-  
-  if (localStorage['greetCount']){
-     greetCounter = Number(localStorage['greetCount']);
-    counterSection.innerHTML = greetCounter;
-  }
-
-  var greetObject = GreetingsFactory();
-
-
+  var storedUsers = localStorage.getItem('Users') ? JSON.parse(localStorage.getItem('Users') ): {};
+  var greetObject = GreetingsFactory(storedUsers);
+  counterSection.innerHTML = greetObject.getCountTotal();
   greetBtn.addEventListener('click',function(){
     var languageButtonChecked = document.querySelector("input[name='greetType']:checked");
+    if(nameBox.value===""){
+        alert('Please enter your name');
+    }
     if (languageButtonChecked){
       var language = languageButtonChecked.value;
       var name = nameBox.value;
-      var greeting = greetObject.greet(name,language);
-      greetCounter++;
-      localStorage['greetCount'] = greetCounter;
+      outputSection.innerHTML = greetObject.greet(name,language);
+      counterSection.innerHTML = greetObject.getCountTotal();
       nameBox.value = "";
-      outputSection.innerHTML = greeting;
-      counterSection.innerHTML = greetCounter;
+      console.log(greetObject.userMap());
+      localStorage.setItem('Users', JSON.stringify(greetObject.userMap()));
+    }else{
+      outputSection.innerHTML = alert('Please choose a language');
     }
   });
+
   resetBtn.addEventListener('click',function(){
     greetObject.reset();
   });
