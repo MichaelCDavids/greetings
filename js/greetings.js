78147dcb4e1e4a0af95f754803ce3984f4e26d3f
michael <michaelcdavids@gmail.com>
2018-05-07 10:38:50 +0200

    fixed bug with empty name field

--- js/greetings.js
+++ js/greetings.js
@@ -9,20 +9,25 @@ document.addEventListener('DOMContentLoaded',function(){
   var resetBtn = document.querySelector('.resetButton');
 
   var storedUsers = localStorage.getItem('Users') ? JSON.parse(localStorage.getItem('Users') ): {};
+
   var greetObject = GreetingsFactory(storedUsers);
+
   counterSection.innerHTML = greetObject.getCountTotal();
+
   greetBtn.addEventListener('click',function(){
     var languageButtonChecked = document.querySelector("input[name='greetType']:checked");
-    if(nameBox.value===""){
-        alert('Please enter your name');
-    }
+
     if (languageButtonChecked){
       var language = languageButtonChecked.value;
       var name = nameBox.value;
-      outputSection.innerHTML = greetObject.greet(name,language);
+      if(name !== ""){
+        outputSection.innerHTML = greetObject.greet(name,language);
+      }else if (name === "" ){
+        alert("Please enter your name");
+        outputSection.innerHTML = "Name can not be empty"
+      }
       counterSection.innerHTML = greetObject.getCountTotal();
-      nameBox.value = "";
-      console.log(greetObject.userMap());
+      nameBox.value = ""
       localStorage.setItem('Users', JSON.stringify(greetObject.userMap()));
     }else{
       outputSection.innerHTML = alert('Please choose a language');
